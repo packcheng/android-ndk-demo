975fc68414159f39f0a71ff29dbc9196512c09e1
packcheng <packcheng_jo@outlook.com>
2021-09-02 21:43:27 +0800

    feat: 异常处理-更新头文件

--- app/src/main/cpp/T3/T3.h
+++ app/src/main/cpp/T3/T3.h
@@ -3,6 +3,7 @@
 //
 
 #ifndef NDKDEMO_T3_H
+#define NDKDEMO_T3_H
 
 #include <jni.h>
 #include "../my-jni.h"
@@ -10,9 +11,9 @@
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_packcheng_ndkdemo_t3_T3_callNativeException(JNIEnv *, jobject);
-#endif //NDKDEMO_T3_H
 
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_packcheng_ndkdemo_t3_T3_callJavaThrowException(JNIEnv *, jobject);
-#define NDKDEMO_T3_H
+
+#endif //NDKDEMO_T3_H
